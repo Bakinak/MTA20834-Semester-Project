c7bd397424c63ef4d7e9b0824063f17ef3a0e296
Hendrik Knoche <hendrik.knoche@gmail.com>
2020-06-19 16:07:12 +0200

    with analysis split by pref

--- DataImportAndShaping.R
+++ DataImportAndShaping.R
@@ -6,16 +6,19 @@ allData <- read_csv("MTA20834 Semester Project/logFiles/allData.csv")
 df1 <- setNames(df1, c("Time", "PID", "PC_Discrete", "PC_Continuous", "CompCtrl_Overall", "ReasonsOther", "Comments", "preferCond"))
 df2 <- setNames(df2, c("Time", "PID", "condition", "PC_KeySeq", "PC_FishReel"))
 
+pref<-df1[,c("PID","preferCond")]
 df1 <- df1 %>%
   select(c(2:5)) %>%
   pivot_longer(-PID, names_sep = "_", names_to = c("rating", "condition"), values_to = "value") %>%
   mutate(context = "All")
 
+
+
 df2 <- df2 %>%
   select(c(2:5)) %>%
   pivot_longer(-c(PID, condition), names_sep = "_", names_to = c("rating", "context"), values_to = "value")
 
-df <- rbind(df1, df2)
+df <- df1 %>% rbind(df2) 
 df1 <- NULL
 df2 <- NULL
 
@@ -38,6 +41,7 @@ dfAgg <- allData %>%
 dfAgg <- dfAgg %>% pivot_longer(-c(PID, condition), names_sep = "_", names_to = c("context", "rating"), values_to = "value")
 dfAgg <- dfAgg[, c(1, 4, 2, 5, 3)]
 dfAll <- rbind(data.frame(dfAgg), data.frame(df))
+dfAll <- dfAll %>%  merge(pref,by="PID")
 
 dfAllWide <- dfAll %>% pivot_wider(names_from = c(rating, context), values_from = value)
 dfAllWide <- dfAllWide %>%
